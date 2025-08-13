874f98573c5220c3186b1cc00ee1369caf80f0ec
David Butcher <dbutcher@spautotech.co.uk>
2025-08-13 14:44:26 +0100

    Added project files

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Oct.EOL.Results.Viewer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
